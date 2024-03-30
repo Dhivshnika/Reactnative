dd2120e5f82fc4a49525fdbd10b69d063ca76d13
Dhivshnika <dhivshnikaselvaraj002@gmail.com>
2024-03-30 23:16:13 +0530

    'change'

---
+++ AuralAssist/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+import Home from "./src/screens/home";
+
+function App() {
+  return (
+    <View>
+      <Home />
+    </View>
+  )
+}
+export default App;
