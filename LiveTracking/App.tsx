66c7b79c21238afa293fc3def62766e15ec24c9f
Dhivshnika <dhivshnikaselvaraj002@gmail.com>
2024-03-31 12:07:45 +0530

    'pushed'

---
+++ LiveTracking/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Welcome from "./src/screen/Welcome/Welcome";
+
+function App() {
+  return (
+    <Welcome />
+  )
+}
+export default App;
